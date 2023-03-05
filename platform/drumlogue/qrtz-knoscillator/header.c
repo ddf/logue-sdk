a464718fccc90a023f455e9dcdbd47be806744bb
Damien Quartz <dcomposer@gmail.com>
2023-03-04 23:07:51 -0800

    fix missing params and reduce max fm ratio because going up super high doesn't make much sense

--- platform/drumlogue/qrtz-knoscillator/header.c
+++ platform/drumlogue/qrtz-knoscillator/header.c
@@ -19,7 +19,7 @@ const __unit_header unit_header_t unit_header = {
     .version = 0x00010000U,                                // This unit's version: major.minor.patch (major<<16 minor<<8 patch).
     .name = "Knoscillator",                                       // Name for this unit, will be displayed on device
     .num_presets = 0,                                      // Number of internal presets this unit has
-    .num_params = 6,                                       // Number of parameters for this unit, max 24
+    .num_params = 8,                                       // Number of parameters for this unit, max 24
     .params = {
         // Format: min, max, center, default, type, fractional, frac. type, <reserved>, name
 
@@ -34,7 +34,7 @@ const __unit_header unit_header_t unit_header = {
         // Page 2
         {0, 100, 0, 0, k_unit_param_type_percent, 0, 0, 0, {"MORPH"}},
         {0, 100, 0, 0, k_unit_param_type_percent, 0, 0, 0, {"FM INDEX"}},
-        {0, 18, 0, 4, k_unit_param_type_strings, 0, 0, 0, {"FM RATIO"}},
+        {0, 10, 0, 4, k_unit_param_type_strings, 0, 0, 0, {"FM RATIO"}},
         {0, 100, 0, 0, k_unit_param_type_percent, 0, 0, 0, {"NOISE"}},
 
         // Page 3
@@ -59,4 +59,6 @@ const __unit_header unit_header_t unit_header = {
         {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
         {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
         {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}},
-        {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}}}};
+        {0, 0, 0, 0, k_unit_param_type_none, 0, 0, 0, {""}}
+    }
+};
